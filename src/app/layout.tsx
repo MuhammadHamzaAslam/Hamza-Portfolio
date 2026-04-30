660c78cbeb448e904d93b027966a61611ea98109
Hamza <153912992+MuhammadHamzaAslam@users.noreply.github.com>
2026-04-30 20:44:37 +0500

    deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,20 @@
 import type React from "react";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import favicon from "../../public/images/favicon.webp";
 import CustomCursor from "@/components/cursor";
-import { Analytics } from "@vercel/analytics/next"
 
-const inter = Inter({ subsets: ["latin"] });
+// Font configuration
+const jakarta = Plus_Jakarta_Sans({ 
+  subsets: ["latin"],
+  weight: ['400', '500', '600', '700', '800'], 
+});
 
 export const metadata: Metadata = {
   title: "M Hamza - Web & Mobile App Developer",
-  description: "Personal portfolio of M Hamza, Web & Mobile App Developer",
+  description: "Personal portfolio of Muhammad Hamza, Full-Stack MERN & React Native Developer",
   icons: {
     icon: "https://ideogram.ai/assets/image/lossless/response/OfA5v_40R_6DjR2Kj7CfhQ",
   },
@@ -24,11 +27,12 @@ export default function RootLayout({
 }>) {  
   return (
     <html lang="en" suppressHydrationWarning>
-      <head title="M Hamza - Web & Mobile App Developer">
+      <head>
         <link rel="icon" type="image/x-icon" href={favicon.src}></link>
       </head>
 
-      <body className={inter.className}>
+      {/* Font class apply yahan hogi */}
+      <body className={jakarta.className}>
         <CustomCursor />
         <ThemeProvider
           attribute="class"
